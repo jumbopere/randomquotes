0af448c496823704cf7869ae06aa64e5271a51dd
Jumbo Pere <jumboperbara0@gmail.com>
2022-10-14 21:04:41 +0100

    remove vitest

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,8 @@
-/// <reference types="vitest" />
-
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { defineConfig } from 'vitest/config'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  test: {
-    setupFiles: ['./vitest.setup.js'],
-  },
+  plugins: [react()]
 })
+
